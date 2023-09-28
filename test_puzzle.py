a88214759de881a7ed12e485531dff5692626419
Maria Gancayco <gancaycomaria@gmail.com>
2023-09-27 22:52:06 -0400

    Implement and test move left.

--- test_puzzle.py
+++ test_puzzle.py
@@ -62,5 +62,35 @@ class TestPuzzleMethods(unittest.TestCase):
         initial = PuzzleState([1,2,3,4,5,6,7,8,0], 3)
         self.assertIsNone(initial.move_down())
         
+    def test_move_blank_left_in_left_col(self):
+        initial = PuzzleState([0,1,2,3,4,5,6,7,8], 3)
+        self.assertIsNone(initial.move_left())
+        
+        initial = PuzzleState([1,2,3,0,4,5,6,7,8], 3)
+        self.assertIsNone(initial.move_left())
+        
+        initial = PuzzleState([1,2,3,4,5,6,0,7,8], 3)
+        self.assertIsNone(initial.move_left())
+        
+    def test_move_blank_left_in_middle_col(self):
+        initial = PuzzleState([1,0,2,3,4,5,6,7,8], 3)
+        self.assertEqual(initial.move_left().config, [0,1,2,3,4,5,6,7,8])
+        
+        initial = PuzzleState([1,2,3,4,0,5,6,7,8], 3)
+        self.assertEqual(initial.move_left().config, [1,2,3,0,4,5,6,7,8])
+        
+        initial = PuzzleState([1,2,3,4,5,6,7,0,8], 3)
+        self.assertEqual(initial.move_left().config, [1,2,3,4,5,6,0,7,8])
+
+    def test_move_blank_left_in_right_col(self):
+        initial = PuzzleState([1,2,0,3,4,5,6,7,8], 3)
+        self.assertEqual(initial.move_left().config, [1,0,2,3,4,5,6,7,8])
+        
+        initial = PuzzleState([1,2,3,4,5,0,6,7,8], 3)
+        self.assertEqual(initial.move_left().config, [1,2,3,4,0,5,6,7,8])
+        
+        initial = PuzzleState([1,2,3,4,5,6,7,8,0], 3)
+        self.assertEqual(initial.move_left().config, [1,2,3,4,5,6,7,0,8])
+        
 if __name__ == '__main__':
     unittest.main()
